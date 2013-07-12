7b2f30f698fe898d3bdc9e1ff00c8b53b1898bf6
Jant Guerrero <example@stanford.edu>
2013-07-12 19:55:48 +0000

    added web.js + index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var helloFile = fs.readFileSync('index.html');
+var hello = helloFile.toString('utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
